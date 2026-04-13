b66dc1c31046ee54eb5ed238ae3041b5c5d90651
Dhruv Goswami <goswamidhruv088@gmail.com>
2026-04-13 19:08:40 +0530

    feat:Function : creating Reusable Logics !!

--- firstcode.py
+++ firstcode.py
@@ -1,10 +1,6 @@
- #Automation : Task Processing Simulation 
-pending_tasks = ["clean the room","clean the desk","update windows","practice DSA"]
- 
-print("Starting Daily tasks varification...")
-
-#using for loop 
-for task in pending_tasks:
-    print( f"Checking task : {task}...[completed]")
-    
-print("All scheduled tasks for today have been varified !")    
+# Functions: creating reusable logics  
+def age_verification(years):
+    if years >= 18 :
+        return True
+    else:
+        return False
