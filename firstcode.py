556e80773103188f32a2b314ba4c7a46ec084167
Dhruv Goswami <goswamidhruv088@gmail.com>
2026-04-14 07:23:08 +0530

    feat : implement dictionary structures for system configuration

--- firstcode.py
+++ firstcode.py
@@ -1,6 +1,15 @@
-# Functions: creating reusable logics  
-def age_verification(years):
-    if years >= 18 :
-        return True
-    else:
-        return False
+# Dictionaries : Managing System Configuration
+System_config = {"server_name" : "host - 1",
+                 "uptime_days" : 45 ,
+                 "is_active" : True ,
+                 "authorised_users": ["admin","lead-dev","devops"]
+                }
+
+# Accessing specific data using keys
+print(f"Connecting to:{System_config['server_name']}")
+
+#Adding a new key-value pair to ditionary
+System_config["max_connections"] = 100
+print(f"Max connections allowed: {System_config['max_connections']}")
+
+print(f"system status : {System_config}")
