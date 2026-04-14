9c6d93386fed0884045ec058f1d0552471dd1833
Dhruv Goswami <goswamidhruv088@gmail.com>
2026-04-14 07:46:49 +0530

    feat : implement file I/O for persistent system logging

--- firstcode.py
+++ firstcode.py
@@ -13,3 +13,16 @@ System_config["max_connections"] = 100
 print(f"Max connections allowed: {System_config['max_connections']}")
 
 print(f"system status : {System_config}")
+
+#File I/O : Reading and writing log files
+
+#writing to a log file
+with open("system_log.txt","w") as log_file:
+    log_file.write("system started successfully\n")
+    log_file.write("uptime : 45 days\n")
+
+#reading from a log file
+with open("system_log.txt","r") as log_file:
+     log_content = log_file.read()
+     print("----- Retrieved log data -----")
+     print(log_content)
